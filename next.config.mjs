01ba4472646ab59a4308e2a0a2e9fdb0012e6f0b
Carlos Hernandez <carlos.hernandez.agudelo@correounivalle.edu.co>
2024-04-16 15:50:48 -0500

    Add the initial structure of the project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['images.pexels.com'],
+    domains: ['github.com'],
+  },
+};
 
 export default nextConfig;
---
+++ src/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Lusitana, Montserrat } from 'next/font/google'; //Import any google font here
+
+export const montserrant = Montserrat({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400'],
+  subsets: ['latin'],
+});
