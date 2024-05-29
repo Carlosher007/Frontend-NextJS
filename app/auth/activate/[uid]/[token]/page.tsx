32ff1ba35708d95b1e230a22adcf0bd1bc724933
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2024-05-29 13:17:25 -0500

    Added account activation page

--- app/auth/activate/[uid]/[token]/page.tsx
+++ app/auth/activate/[uid]/[token]/page.tsx
@@ -1,5 +1,25 @@
 "use client"
 
+import Link from "next/link";
+
 export default function Page({ params }: { params: { uid: string, token:string } }) {
-    return <div>My Post: {params.uid} and {params.token}</div>
+    const { uid, token } = params;
+    
+    return (
+        <div className="p-4 flex flex-col justify-center items-center min-h-screen">
+            <p className="font-bold text-2xl"> ImageHub </p>
+            <div className="pt-10 max-w-auto justify-center items-center mx-auto text-center">
+                <p className="text-4xl font-bold">You are so close to be part of us!</p>
+                <p>To finish the process, click in the following link</p>
+                <br/>
+                <br/>
+                <Link href={`/`} className="text-blue-500">Activate your account</Link>
+                <br/>
+                <br/>
+                <br/>
+                <p>After you click the link, you'll be able to sign in the app</p>
+                <p className="text-small text-gray-400">Welcome to ImageHub Family!</p>
+            </div>
+        </div>
+    )
 }
